1b3e8529eab299864718a292ea58890fc6894207
scpcom <scpcom@gmx.de>
2026-04-24 00:50:29 +0200

    thirdparty: get nlohmannjson and stb from git

--- cmake/thirdparty.cmake
+++ cmake/thirdparty.cmake
@@ -81,6 +81,8 @@ if(NOT IS_DIRECTORY "${BUILD_DOWNLOAD_DIR}/nlohmannjson-src")
   FetchContent_Declare(
     nlohmannjson
     URL ${NLOHMANNJSON_URL}
+    GIT_REPOSITORY https://github.com/nlohmann/json.git
+    GIT_TAG v3.11.3
   )
   FetchContent_MakeAvailable(nlohmannjson)
   message("Content downloaded to ${nlohmannjson_SOURCE_DIR}")
@@ -97,7 +99,8 @@ endif()
 if(NOT IS_DIRECTORY "${BUILD_DOWNLOAD_DIR}/stb-src")
   FetchContent_Declare(
     stb
-    URL ${STB_URL}
+    GIT_REPOSITORY https://github.com/nothings/stb.git
+    GIT_TAG origin/master
   )
   FetchContent_MakeAvailable(stb)
   message("Content downloaded to ${stb_SOURCE_DIR}")
