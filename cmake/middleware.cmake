f7396feeb26bfe070ba6ac8786a199b00b0502ab
scpcom <scpcom@gmx.de>
2026-04-25 17:16:30 +0200

    middleware: remove libsbc

--- cmake/middleware.cmake
+++ cmake/middleware.cmake
@@ -351,9 +351,7 @@ set(MIDDLEWARE_AUDIO_LIBS
     ${MIDDLEWARE_SDK_ROOT}/lib/libcvi_dnvqe.so
     ${MIDDLEWARE_SDK_ROOT}/lib/libcvi_VoiceEngine.so
     ${MIDDLEWARE_SDK_ROOT}/lib/libcvi_RES1.so
-    ${MIDDLEWARE_SDK_ROOT}/lib/libtinyalsa.so
-    $<$<STREQUAL:${CVI_PLATFORM},CV180X>:${MIDDLEWARE_SDK_ROOT}/lib/3rd/libsbc.so>
-    $<$<STREQUAL:${CVI_PLATFORM},CV181X>:${MIDDLEWARE_SDK_ROOT}/lib/3rd/libsbc.so>)
+    ${MIDDLEWARE_SDK_ROOT}/lib/libtinyalsa.so)
 
 set(MIDDLEWARE_AUDIO_LIBS_STATIC
     "-Wl,--whole-archive"
@@ -363,8 +361,6 @@ set(MIDDLEWARE_AUDIO_LIBS_STATIC
     ${MIDDLEWARE_SDK_ROOT}/lib/libcvi_VoiceEngine.a
     ${MIDDLEWARE_SDK_ROOT}/lib/libcvi_RES1.a
     ${MIDDLEWARE_SDK_ROOT}/lib/libtinyalsa.a
-    $<$<STREQUAL:${CVI_PLATFORM},CV180X>:${MIDDLEWARE_SDK_ROOT}/lib/3rd/libsbc.a>
-    $<$<STREQUAL:${CVI_PLATFORM},CV181X>:${MIDDLEWARE_SDK_ROOT}/lib/3rd/libsbc.a>
     "-Wl,--no-whole-archive")
 
 message("MIDDLEWARE_INCLUDES: ${MIDDLEWARE_INCLUDES}")
